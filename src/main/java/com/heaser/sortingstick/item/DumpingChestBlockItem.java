2fae30f80fc61d2d912fdbed9647391b43e8bfbb
Heaser <heasos@gmail.com>
2026-04-05 17:58:07 +0300

    Added a tooltip to the dumping chest and fixed loottable

--- src/main/java/com/heaser/sortingstick/item/DumpingChestBlockItem.java
+++ src/main/java/com/heaser/sortingstick/item/DumpingChestBlockItem.java
@@ -2,17 +2,27 @@ package com.heaser.sortingstick.item;
 
 import com.heaser.sortingstick.client.DumpingChestBEWLR;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class DumpingChestBlockItem extends BlockItem {
 
     public DumpingChestBlockItem(Block block, Item.Properties properties) {
         super(block, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag flag) {
+        tooltipComponents.add(Component.translatable("item.sortingstick.dumping_chest.tooltip"));
+        super.appendHoverText(stack, context, tooltipComponents, flag);
     }
 
     @Override
